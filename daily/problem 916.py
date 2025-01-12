4197aa9235ad99246e006bfde69b11b2a1fb76b7
Arnav Gupta <arnavgupta9000@gmail.com>
2025-01-12 14:41:15 -0700

    end of jan 11

--- daily/problem 916.py
+++ daily/problem 916.py
@@ -11,42 +11,55 @@ Return an array of all the universal strings in words1. You may return the answe
 
 def solve(words1, words2):    
     res = []
-
-    n = len(words1)
-    n2 = len(words2)
-
+    hash2 = {}
+    for j in words2:
+        for k in j:
+            hash2[k] = hash2.get(k,0) + 1
+        
     for i in words1:
         hash1 = {}
-        hash2 = {}
-        for j in words2:
-            hash2[j] = hash2.get(j,0) + 1
-        
-        # for k in i:
-        #     if k in hash2:
-        #         hash1[k] = hash1.get(k,0) +1
-        #         if hash1[k] > hash2[k]:
-        #             hash1[k] = hash2[k]
-        # if hash1 == hash2:
-        #     res.append(i)
-        var = True
-        for key, value in hash2.items():
-            x = 0
-            while value > 0 and var:
-                x = i.find(key, x)
-                if x == -1:
-                    var = False
-                value -= 1
-        if var:
+       
+        for k in i:
+            if k in hash2:
+                hash1[k] = hash1.get(k,0) +1
+                if hash1[k] > hash2[k]:
+                    hash1[k] = hash2[k]
+        if hash1 == hash2:
             res.append(i)
+       
+    return res
+from collections import defaultdict
+from collections import Counter
+
+def solve2(words1, words2):
+    count_2 = defaultdict(int)
+    for w in words2:
+        count_w = Counter(w)
+        for c, cnt in count_w.items():
+            count_2[c] = max(count_2[c], cnt)
+    res = []
+
+    for w in words1:
+        count_w = Counter(w)
+        flag = True
+        for c, cnt in count_2.items():
+            if count_w[c] < cnt:
+                flag = False
+                break
+        if flag:
+            res.append(w)
+
     return res
 
-print(solve(["amazon","apple","facebook","google","leetcode"], ["lo","eo"])) 
 
+print(solve2(["amazon","apple","facebook","google","leetcode"], ["lo","eo"])) 
 
 '''
 medium problem
 
 idea: O(n * m) but make hashmaps since order of subsets doenst matter (ie "wr" is a subset of "rw")
 
-idk... to many weird things..
+idk... to many weird things.. had to watch the video
+
+this idea with the sol is to merge the hashmaps by taking the max of each letter freq NOT ADDING. i was super close myself lol
 '''
